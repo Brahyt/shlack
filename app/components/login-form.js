127b930f3bf45df54b460c52f1150345be213a18
Daniel Bright <brahyt@gmail.com>
2020-03-06 11:31:07 -0800

    added intigration test for form

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,4 +1,20 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class LoginFormComponent extends Component {
+  loginWithUserId(val) {
+    console.log(val);
+  }
+
+  /*
+   *@param {Event & {target: HTMLFormElement}} e
+   */
+
+  @action //binds this to the object it lives on
+  onLoginFormSubmit(e) {
+    e.preventDefault();
+    const { target } = e;
+    const val = target.querySelector('select').value;
+    this.loginWithUserId(val);
+  }
 }
